fbb62034d9ebda168453b47d9c62de54782057af
TiagoDiass <tiago.costadiasss@gmail.com>
2020-09-01 12:50:41 -0300

    comentário adicionado

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
-  
+/* Professor, estou com um computador um pouco lento por enquanto, por isso, não instalei o ambiente Java na minha
+ máquina pois não consigo usar nem Javascript direito, quanto mais Java. Porém escrevi as classes com meus conhecimentos de OO
+ que adquiri no Javascript(e comparando com seus exemplos pra ver se estava certo) mas não executei o código pra testar, creio
+ que o senhor conseguirá ver que ele está certo só de ler*/
 package net.tiago.prjapolice;
 
 public class Main {
 
     public static void main(String[] args) {
+        Apolice apolice = new Apolice('Tiago Dias', 18, 599);
+        apolice.imprimir();
     }
 }
